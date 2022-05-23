d5881b169b937ac528fed08791700426e8f904b4
Spud97 <luca.dercole97@gmail.com>
2022-05-23 19:13:26 +0200

    tentativo

---
+++ Ex.1/js/main.js
@@ -0,0 +1,15 @@
+let emailUtente = prompt("Inserisci la tua E-Mail");
+let emailList = [
+  "MarcoPolo@gmail.com",
+  "DanteAlighieri@gmail.com",
+  "LeonardoDaVinci@gmail.com",
+  "GiuseppeVerdi@gmail.com",
+];
+
+for (let i = 0; i < emailList.length; i++) {
+  if (emailUtente === emailList[i]) {
+    alert("E-Mail valida.");
+  } else {
+    alert("E-Mail non valida, riprova.");
+  }
+}
---
+++ Ex.2/js/main.js
@@ -0,0 +1,9 @@
+let dado1 = [Math.round(Math.random()*5)+1]
+let dado2 = [Math.round(Math.random()*5)+1]
+if (dado1 = dado2) {
+    alert(Pareggio)
+} else if (dado1 > dado2) {
+    alert(Vittoria del Player)
+} else {
+    alert(vittoria del UI)
+}
